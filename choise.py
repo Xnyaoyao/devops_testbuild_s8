e6b8433cef9278f2ecd1b5b196efbc2eb51ef55c
Xnyaoyao <feng@et.esiea.fr>
2022-03-16 11:26:28 +0100

    choise file

--- choise.py
+++ choise.py
@@ -71,7 +71,8 @@ fout = open("index.php", "wt")
 # for each line in the input file
 for line in fin:
     fout.write(line.replace('question1', words[firstquestion])
-        .replace('question2', words[secondquestion]).replace('question3', words[thirdquestion]))
+                   .replace('question2', words[secondquestion])
+                   .replace('question3', words[thirdquestion]))
 # close input and output files
 fin.close()
 fout.close()
